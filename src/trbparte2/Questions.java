379577cca99966853b302442e704c285774188f5
pedroeveling <38307430+pedroeveling@users.noreply.github.com>
2018-05-23 13:08:46 -0300

    Add files via upload

---
+++ src/trbparte2/Questions.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed2_pt2;
+
+
+public class Questions {
+    String id;
+    String user,data,score,title;
+
+    public Questions(String id, String user, String data, String score, String title) {
+        this.id = id;
+        this.user = user;
+        this.data = data;
+        this.score = score;
+        this.title = title;
+    }
+    
+   void imprime()
+   {
+        System.out.println("id "+id);
+        System.out.println("usr " +user);
+        System.out.println("data " +data);
+        System.out.println("score "+score);
+        System.out.println("title "+title);
+   }
+}
