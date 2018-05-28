eb41469e3b7ce73d95c2a675dbcddfc17bc18ba3
rafaelbrjf <31705748+rafaelbrjf@users.noreply.github.com>
2018-05-27 23:53:11 -0300

    Switch case

--- src/trbparte2/InsereRemove.java
+++ src/trbparte2/InsereRemove.java
@@ -5,31 +5,49 @@ public class InsereRemove extends OperadorQuestions
 {
     int x;
 
-    public InsereRemove(int x) 
+    public InsereRemove(int x, int op) 
     {
         super(x);
         this.x=x;
-        long inicioavl = System.currentTimeMillis();
-        InsAVL();
-        System.out.println("A insercao da AVL executou em " + (System.currentTimeMillis() - inicioavl)/1000 + " segundos");
-        long iniciob = System.currentTimeMillis();
-        InsArvoreB();
-        System.out.println("A insercao da Arvore B executou em " + (System.currentTimeMillis() - iniciob)/1000 + " segundos");
-        long iniciosplay = System.currentTimeMillis();
-        InsArvoreSplay();
-        System.out.println("A insercao da Arvore Splay executou em " + (System.currentTimeMillis() - iniciosplay)/1000 + " segundos");
-        long iniciovp = System.currentTimeMillis();
-        InsArvoreVP();
-        System.out.println("A insercao da Arvore VP executou em " + (System.currentTimeMillis() - iniciovp)/1000 + " segundos");        
-        long inicioREMavl = System.currentTimeMillis();
-        RemAVL();
-        System.out.println("A remocao da AVL executou em " + (System.currentTimeMillis() - inicioREMavl)/1000 + " segundos");
-        long inicioREMb = System.currentTimeMillis();
-        RemArvoreB();
-        System.out.println("A remocao da Arvore B executou em " + (System.currentTimeMillis() - inicioREMb)/1000 + " segundos");
-        long inicioREMvp = System.currentTimeMillis();
-        RemArvoreVP();
-        System.out.println("A remocao da Arvore VP executou em " + (System.currentTimeMillis() - inicioREMvp)/1000 + " segundos");
+        switch(op){
+            case 1:
+                long inicioavl = System.currentTimeMillis();
+                InsAVL();
+                System.out.println("A insercao da AVL executou em " + (System.currentTimeMillis() - inicioavl)/1000 + " segundos");
+                break;
+            case 2:    
+                long iniciob = System.currentTimeMillis();
+                InsArvoreB();
+                System.out.println("A insercao da Arvore B executou em " + (System.currentTimeMillis() - iniciob)/1000 + " segundos");
+                break;
+            case 3:
+                long iniciosplay = System.currentTimeMillis();
+                InsArvoreSplay();
+                System.out.println("A insercao da Arvore Splay executou em " + (System.currentTimeMillis() - iniciosplay)/1000 + " segundos");
+                break;
+            case 4:    
+                long iniciovp = System.currentTimeMillis();
+                InsArvoreVP();
+                System.out.println("A insercao da Arvore VP executou em " + (System.currentTimeMillis() - iniciovp)/1000 + " segundos");        
+                break;
+            case 5:
+                long inicioREMavl = System.currentTimeMillis();
+                RemAVL();
+                System.out.println("A remocao da AVL executou em " + (System.currentTimeMillis() - inicioREMavl)/1000 + " segundos");
+                break;
+            case 6:
+                long inicioREMb = System.currentTimeMillis();
+                RemArvoreB();
+                System.out.println("A remocao da Arvore B executou em " + (System.currentTimeMillis() - inicioREMb)/1000 + " segundos");
+                break;
+            case 7:
+                long inicioREMvp = System.currentTimeMillis();
+                RemArvoreVP();
+                System.out.println("A remocao da Arvore VP executou em " + (System.currentTimeMillis() - inicioREMvp)/1000 + " segundos");
+                break;
+            default:
+                System.out.println("Numero invalido");
+        }
     }
     
     void setx(int x)
